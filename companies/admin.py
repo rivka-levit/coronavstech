16686f97db3d979d2a7b36f4f12d6d9418d13c01
rivka-yuliia <juliacreadora@gmail.com>
2024-10-15 06:58:43 +0300

    Company admin register

--- companies/admin.py
+++ companies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from companies.models import Company
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    pass
--- companies/models.py
+++ companies/models.py
@@ -3,6 +3,10 @@ from django.utils.timezone import now
 
 
 class Company(models.Model):
+    class Meta:
+        verbose_name = 'company'
+        verbose_name_plural = 'companies'
+
     class CompanyStatus(models.TextChoices):
         LAYOFFS = 'Layoffs'
         HIRING_FREEZE = 'Hiring Freeze'
@@ -15,3 +19,6 @@ class Company(models.Model):
     last_updated = models.DateTimeField(default=now, editable=True)
     app_link = models.URLField(blank=True)
     notes = models.CharField(max_length=1000, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
