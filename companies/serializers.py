c8db97cc9f5024ba64e3a5d78ffe333a6dadd4d9
rivka-yuliia <juliacreadora@gmail.com>
2024-10-16 05:43:51 +0300

    Company serializer

---
+++ companies/serializers.py
@@ -0,0 +1,13 @@
+"""
+Serializers for Company app
+"""
+
+from rest_framework import serializers
+from companies.models import Company
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ['id', 'name', 'status', 'app_link', 'last_updated', 'notes']
+        read_only_fields = ['id']
