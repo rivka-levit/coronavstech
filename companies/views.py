eb17c4a577f7a727068ff6b07cc94ae334cde40a
rivka-yuliia <juliacreadora@gmail.com>
2024-10-16 05:50:56 +0300

    Company viewset

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from companies.models import Company
+from companies.serializers import CompanySerializer
+
+
+class CompanyViewSet(ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all().order_by('-last_updated')
+    pagination_class = PageNumberPagination
