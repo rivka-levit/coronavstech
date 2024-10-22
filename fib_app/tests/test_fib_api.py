a865893136eecaecff57649d5992939e2cc1b702
rivka-yuliia <juliacreadora@gmail.com>
2024-10-22 04:50:08 +0300

    test fib_abi with big numbers

--- fib_app/tests/test_fib_api.py
+++ fib_app/tests/test_fib_api.py
@@ -64,3 +64,22 @@ def test_post_method_not_allowed(client) -> None:
 
     assert r.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
     assert r.data['detail'] == 'Method "POST" not allowed.'
+
+
+@pytest.mark.parametrize(
+    argnames='n, expected',
+    argvalues=[
+        (125, 59425114757512643212875125),
+        (258, 370959230771131880927453318055001997489772178180790104),
+        (1000, 43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875)
+    ]
+)
+def test_stress_big_numbers_success(client, n: int, expected: int) -> None:
+    """Stress test to assure that fib endpoint works with big numbers."""
+
+    r = client.get(f'{BASE_FIB_URL}?n={n}')
+
+    assert r.status_code == status.HTTP_200_OK
+    assert r.data['status'] == 'success'
+    assert r.data['n_requested'] == n
+    assert r.data['fibonacci_number'] == expected
